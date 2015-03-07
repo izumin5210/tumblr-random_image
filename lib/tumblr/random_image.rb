bcc5a6157fe0c94be2f5225c6da0a45c6b804a6d
izumin5210 <masayuki@izumin.info>
2015-03-08 01:21:08 +0900

    Implement Tumblr::RandomImage

--- lib/tumblr/random_image.rb
+++ lib/tumblr/random_image.rb
@@ -1,7 +1,23 @@
-require "tumblr/random_image/version"
+require 'tumblr/random_image/version'
 
 module Tumblr
   module RandomImage
-    # Your code goes here...
+    def random_image(blog_name, options = {})
+      options[:offset] = random_offset(blog_name)
+      options[:limit] = 1
+      posts(blog_name, options)['posts'][0]['photos'][0]['original_size']['url']
+    end
+
+    private
+
+    def last(blog_name, options = {})
+      options[:limit] = 1
+      posts(blog_name, options)
+    end
+
+    def random_offset(blog_name, options = {})
+      total_posts = last(blog_name, options)['total_posts']
+      rand(1..total_posts).round
+    end
   end
 end
